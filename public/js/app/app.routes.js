a0dd65620dbe53c5ca71b833b3ac1cd48b862837
myunghoon kim <myungtkim@gmail.com>
2016-04-02 19:13:48 -0700

    creates this app's routing via app.routes.js. adds in initial state for landing and related template

---
+++ public/js/app/app.routes.js
@@ -0,0 +1,20 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("Momentum")
+    .config(AppRoutes);
+
+  AppRoutes.$inject = ["$stateProvider", "$urlRouterProvider"];
+
+  function AppRoutes($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("landing", {
+        url: "/",
+        templateUrl: "js/app/templates/landing.html"
+      });
+
+    $urlRouterProvider.otherwise("/");
+  }
+
+})();
