894cb2885b11cd38dee84a1d5456ee7ea2f03d6c
myunghoon kim <myungtkim@gmail.com>
2016-04-03 22:04:15 -0700

    works on basic structure for moment synthesizer

--- public/js/app/controller/momentum.controller.js
+++ public/js/app/controller/momentum.controller.js
@@ -13,23 +13,56 @@
 
         "use strict";
 
-    var scene=new THREE.Scene(),
-        light= new THREE.AmbientLight(0xffffff),
+    var scene       = new THREE.Scene(),
+        light       = new THREE.AmbientLight(0xffffff),
         renderer,
-        camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000 ),
-        renderer = new THREE.WebGLRenderer(),
-        mouseX = 0,
-        mouseY = 0,
+        camera      = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000 ),
+        renderer    = new THREE.WebGLRenderer(),
+        mouseX      = 0,
+        mouseY      = 0,
         windowHalfX = window.innerWidth / 2,
         windowHalfY = window.innerHeight / 2;
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( 1000, 450 );
     document.getElementById("momentum").appendChild(renderer.domElement);
 
-    var geometry = new THREE.IcosahedronGeometry( 50, 1 );
+    // var geometry = new THREE.IcosahedronGeometry( 50, 1 );
+    var geometry = new THREE.Geometry();
+
+    function rng(max, min, bool) {
+      if (bool) return Math.floor((Math.random() * (max - min)) + min);
+      else return (Math.random() * (max - min)) + min;
+    };
+
+    var rngHeightWidthFactor = rng(0.75, 0.5, false);
+    var rngApexNadir = [ rng(200, 100, true), -(rng(200, 100, true)) ];
+    var rngPoles     = [ rngApexNadir[0] * rngHeightWidthFactor, rngApexNadir[1] * rngHeightWidthFactor ]
+
+    $log.debug(rngPoles)
+
+    geometry.vertices.push(
+      new THREE.Vector3(   0,  rngApexNadir[0],   0 ),
+      new THREE.Vector3(   0, rngApexNadir[1],   0 ),
+      new THREE.Vector3(  rngPoles[0],   0,   rngPoles[0] ),
+      new THREE.Vector3(   rngPoles[0],   40, rngPoles[1] ),
+      new THREE.Vector3( rngPoles[1],   0,   rngPoles[0] ),
+      new THREE.Vector3(   rngPoles[1],   40,  rngPoles[1] )
+    );
+
+    geometry.faces.push( new THREE.Face3( 0, 2, 5 ) );
+    geometry.faces.push( new THREE.Face3( 0, 2, 3 ) );
+    geometry.faces.push( new THREE.Face3( 0, 3, 4 ) );
+    geometry.faces.push( new THREE.Face3( 0, 4, 5 ) );
+    geometry.faces.push( new THREE.Face3( 1, 2, 5 ) );
+    geometry.faces.push( new THREE.Face3( 1, 2, 3 ) );
+    geometry.faces.push( new THREE.Face3( 1, 3, 4 ) );
+    geometry.faces.push( new THREE.Face3( 1, 4, 5 ) );
+
+    geometry.computeBoundingSphere();
+
     var material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true } );
     var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    scene.add( cube, light );
     camera.position.z = 500;
 
 
@@ -48,7 +81,9 @@
       // camera.position.x += ( mouseX - camera.position.x );
       // camera.position.y += ( - mouseY - camera.position.y );
       // camera.lookAt( scene.position );
-      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.005;
+
+      // console.log(cubic-bezier(.17,.67,.83,.67))
 
       renderer.render( scene, camera );
     }
