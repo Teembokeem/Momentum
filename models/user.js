c67aa59000d832aad4a4237240b23d90f0df9baf
myunghoon kim <myungtkim@gmail.com>
2016-04-04 18:50:12 -0700

    adds button for creating a new moment; adds form for creating new moments

--- models/user.js
+++ models/user.js
@@ -1,9 +1,21 @@
 var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
+var Moments = new mongoose.Schema({
+  title: String,
+  text:  String,
+  createdAt: {type: String, default: Date.now},
+  slug: String,
+  images: [{type: String}],
+  isPublished: {type: Boolean, default: false},
+  publishedAt: {type: Date}
+});
+
+
 var userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  name:  { type: String, required: true }
+  name:  { type: String, required: true },
+  moments: [Moments]
 });
 
 // Add bcrypt hashing to model (works on a password field)!
